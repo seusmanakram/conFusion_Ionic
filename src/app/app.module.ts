40620b3699d7089e33c7c513c732282f6bebc213
Usman Akram <usman.akram1996@outlook.com>
2017-12-05 20:05:10 +0500

    Sending Email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,8 @@ import { LoginPage } from '../pages/login/login';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {LocalNotifications} from '@ionic-native/local-notifications';
+import {EmailComposer} from '@ionic-native/email-composer';
+
 import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
 import { PromotionProvider } from '../providers/promotion/promotion';
@@ -71,6 +73,7 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
     StatusBar,
     SplashScreen,
     LocalNotifications,
+    EmailComposer,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     DishProvider,
     LeaderProvider,
